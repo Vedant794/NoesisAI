80be37200e2ce0313d2079be8a60df6d6415f046
Vedant794 <vedantkumbhare721@gmail.com>
2025-01-16 18:08:44 +0530

    LLM Integration

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,3 +1,20 @@
 require("dotenv").config();
+import express from "express";
+import Groq from "groq-sdk";
 
-console.log(process.env.CLUADE_API_KEY);
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function main() {
+  const completion = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "Explain the importance of fast language models",
+      },
+    ],
+    model: "llama-3.3-70b-versatile",
+  });
+  console.log(completion.choices[0].message.content);
+}
+
+main();
